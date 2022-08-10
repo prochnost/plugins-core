2174c129905b11233dd151ae6b328d0bd1e717ae
Anthony Chibinda <chibinda.kangoro@gmail.com>
2022-08-10 17:14:10 +0200

    initial structure

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { PluginManager } from './plugin-manager';
+
+export { Extension } from './extension';
+export const pluginManager = new PluginManager();
---
+++ src/plugin-manager.js
@@ -0,0 +1,13 @@
+import { Extension } from './extension';
+
+export class PluginManager {
+
+  getType(name) {
+    return Extension.prototype.getExtensionType(name);
+  }
+
+  getProviders() {
+    return Extension.prototype.getProviders();
+  }
+
+}
